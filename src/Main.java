4a4032e0ce1ace33fd42918af9b68b52140de91e
Mirlan Ipasov <mipasov@gmail.com>
2021-03-26 08:52:28 +0600

    some useful refactoring

--- src/Main.java
+++ src/Main.java
@@ -17,16 +17,6 @@ public class Main {
         IncomeService incomeService = new IncomeService();
         ExpenseService expenseService = new ExpenseService();
 
-       /* Income temp1 = new Income("Salary", 2000, LocalDateTime.of(2021, 3, 7, 12, 20, 30));
-        Income temp2 = new Income("Freelancing", 1000, LocalDateTime.of(2021, 3, 10, 12, 20, 30));
-        Income temp3 = new Income("Trading", 500, LocalDateTime.of(2021, 1, 21, 12, 20, 30));
-        Expense tempExpense = new Expense("taxi bill", 100.0f, LocalDateTime.of(2021, 1, 21, 12, 20, 30));
-
-        incomeService.insert(temp1);
-        incomeService.insert(temp2);
-        incomeService.insert(temp3);
-        expenseService.insert(tempExpense);
-*/
         do {
             System.out.println(ui.mainMenu());
             int input = scanner.nextInt();
